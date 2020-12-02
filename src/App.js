6653020ccf7e3d807286480ed09f644cba38ceee
gleek908i <am_bsk_love_rubio@yahoo.co.jp>
2020-12-03 00:58:41 +0900

    my first react hook with useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount( count + 1 );
+  const decrement = () => setCount( count - 1 );
+
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+
+  const reset = () => setCount( 0 );
+  
+  const twice = () => setCount( count * 2 );
+
+  const devide3 = () => {
+    if(!(count%3)){
+      setCount(count / 3);
+    }
+  };
+/*const devide3 = () => setCount(previousCount => {
+    if( previousCount % 3 ===0 ) {
+      return previousCount / 3
+    } else {
+      return previousCount;
+    }
+  })
+*/
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div>count: {count}</div>
+          <div>
+              <button onClick={increment}>+1</button>
+              <button onClick={decrement}>-1</button>
+          </div>
+          <div>
+              <button onClick={increment2}>+1</button>
+              <button onClick={decrement2}>-1</button>
+          </div>
+          <div>
+              <button onClick={reset}>Reset</button>
+          </div>
+          <div>
+              <button onClick={twice}>×2</button>
+          </div>
+          <div>
+              <button onClick={devide3}>3の倍数の時だけ3でわる</button>
+          </div>
+      </>
   );
 }
 
